e764dc84099d516670c4968af62c6b52f66fcdd2
OlimpioRey <olimpio.rey@campusdual.com>
2022-11-18 14:23:42 +0100

    Chart_OK

--- demo-frontend/src/app/main/graphic/graphic.component.ts
+++ demo-frontend/src/app/main/graphic/graphic.component.ts
@@ -14,9 +14,6 @@ export class GraphicComponent implements OnInit {
 
   dataChart: Object[];
   data: Map<String, Object>[];
-  
-  
-
 
   constructor(private publicationService: PublicationService) { 
   }
@@ -25,28 +22,13 @@ export class GraphicComponent implements OnInit {
     
     let iniDate: Date = new Date('2022-11-16');
     let endDate: Date = new Date('2023-12-31');
-    
-  
-    
 
     this.publicationService.getDataChart(iniDate, endDate).subscribe((response) => {
- 
       this.data = response
-      
       this.drawGraphic();
-
     });
 
   }
-
-  formatJson(lista: Map<String, Object>) {
-    
-
-
-
-    
-  }
-
 
   drawGraphic(){
     let root = am5.Root.new("chartdiv");
@@ -119,7 +101,6 @@ export class GraphicComponent implements OnInit {
     
       legend.data.push(series);
     }
-   
     
     makeSeries("Deportes", "Deportes", this.data);
     makeSeries("Gastronomía", "Gastronomía", this.data);
@@ -129,47 +110,8 @@ export class GraphicComponent implements OnInit {
     makeSeries("Viajes", "Viajes", this.data);
     makeSeries("Otros", "Otros", this.data);
 
-
     chart.appear(1000, 100);
 
   }
 
 }
-
-
-// let data = [{
-    //   "province": "A Coruña",
-    //   "deportes": 2.5,
-    //   "gastronomia": 2.5,
-    //   "ocio": 2.1,
-    //   "naturaleza": 1,
-    //   "viajes": 0.8,
-    //   "otros": 0.4
-    // }, {
-    //   "province": "Lugo",
-    //   "deportes": 2.6,
-    //   "gastronomia": 2.7,
-    //   "ocio": 2.2,
-    //   "naturaleza": 0.5,
-    //   "viajes": 0.4,
-    //   "otros": 0.3
-    // }, {
-    //   "province": "Ourense",
-    //   "deportes": 2.8,
-    //   "gastronomia": 2.9,
-    //   "ocio": 2.4,
-    //   "naturaleza": 0.3,
-    //   "viajes": 0.9,
-    //   "otros": 0.5
-    // }, {
-    //   "province": "Pontevedra",
-    //   "deportes": 2.8,
-    //   "gastronomia": 2.9,
-    //   "ocio": 2.4,
-    //   "naturaleza": 0.3,
-    //   "viajes": 0.9,
-    //   "otros": 0.5
-    // }]
-
-    
-    // this.dataChart.push = this.publicationService.getDataChart(new Date('2022-11-16'), new Date("2022-12-31"));
